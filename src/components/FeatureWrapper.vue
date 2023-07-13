5e7d47d9e0e1b9174fd37c277004d920f9a3606a
Chavez Harris <codedbychavez@gmail.com>
2023-07-13 19:04:54 -0300

    Initial commit

---
+++ src/components/FeatureWrapper.vue
@@ -0,0 +1,37 @@
+<template>
+  <div v-if="isFeatureFlagEnabled">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ['flagValueChanged'],
+  props: {
+    featureKey: {
+      type: String,
+      required: true,
+    },
+    userObject: {
+      type: Object,
+      required: false,
+      default: {},
+    }
+  },
+  data() {
+    return {
+      isFeatureFlagEnabled: false,
+    };
+  },
+  mounted() {
+    // Check if feature flag is enabled
+    this.configCatClient.getValueAsync(this.featureKey, false, this.userObject).then((value) => {
+      this.isFeatureFlagEnabled = value;
+      this.emits('flagValueChanged', value);
+    });
+  },
+  unmounted() {
+    this.configCatClient.dispose();
+  }
+};
+</script>
---
+++ src/plugins/configcat-plugin.js
@@ -0,0 +1,8 @@
+import * as configcat from 'configcat-js';
+
+export default {
+  install: (app, options) => {
+    let configCatClient = configcat.createClient(options.SDKKey, options.clientOptions);
+    app.config.globalProperties.configCatClient = configCatClient;
+  },
+};
