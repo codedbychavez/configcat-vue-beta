08b11b3e0348ff28632b42b0fca5048bfda13ed6
Chavez Harris <codedbychavez@gmail.com>
2023-07-14 16:58:47 -0300

    Testing hook augment init

--- src/plugins/configcat-plugin.js
+++ src/plugins/configcat-plugin.js
@@ -3,6 +3,12 @@ import * as configcat from 'configcat-js';
 export default {
   install: (app, options) => {
     let configCatClient = configcat.createClient(options.SDKKey, options.clientOptions);
+    // Augment the hooks to the client
+    if (options.hooks.length > 0) {
+      for (hook of options.hook) {
+        console.log('The hook: ', hook)
+      }
+    }
     app.config.globalProperties.configCatClient = configCatClient;
   },
 };
