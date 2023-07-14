eb344c101e5376cf58b27e6298e3428213cf947c
Chavez Harris <codedbychavez@gmail.com>
2023-07-14 18:22:43 -0300

    Updates

--- src/plugins/configcat-plugin.js
+++ src/plugins/configcat-plugin.js
@@ -2,9 +2,7 @@ import * as configcat from 'configcat-js';
 
 export default {
   install: (app, options) => {
-    let configCatClient = configcat.getClient(
-      options.SDKKey, 
-      );
+    let configCatClient = configcat.createClient(options.SDKKey, options.clientOptions);
     app.config.globalProperties.configCatClient = configCatClient;
   },
 };
