cd54d3d05f85bdc8f696d713ac6f727b3ef6a459
Chavez Harris <codedbychavez@gmail.com>
2023-07-14 18:56:44 -0300

    Updates

--- src/plugins/configcat-plugin.js
+++ src/plugins/configcat-plugin.js
@@ -2,11 +2,24 @@ import * as configcat from 'configcat-js';
 
 export default {
   install: (app, options) => {
-    let configCatClient = configcat.createClient(options.SDKKey, options.clientOptions);
+
+    let configCatClient = configcat.getClient(
+      options.SDKKey,
+      configcat.PollingMode.AutoPoll, 
+      options.clientOptions
+    )
+
+    if (options.pollingMode == 'manual') {
+      configCatClient = configcat.getClient(
+        options.SDKKey,
+        configcat.PollingMode.ManualPoll, 
+        options.clientOptions
+      );
+    }
+
     app.config.globalProperties.configCatClient = configCatClient;
   },
 };
-
 
 
 // TODO: Check for invalid hook name before mapping
