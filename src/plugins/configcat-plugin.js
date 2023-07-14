15838f605cd3fd0db2d68f113559a39763066b04
Chavez Harris <codedbychavez@gmail.com>
2023-07-14 17:26:44 -0300

    hooks 101

--- src/plugins/configcat-plugin.js
+++ src/plugins/configcat-plugin.js
@@ -5,10 +5,18 @@ export default {
     let configCatClient = configcat.createClient(options.SDKKey, options.clientOptions);
     // Augment the hooks to the client
     if (options.hooks) {
-      for (const hook of options.hooks) {
-        console.log(typeof configCatClient)
+      for (let hook of options.hooks) {
+        app.config.globalProperties[`${hook}`] = function() {
+          return configCatClient.on(`${hook}`, () => {
+
+          });
+        }
       }
     }
     app.config.globalProperties.configCatClient = configCatClient;
   },
 };
+
+
+
+// TODO: Check for invalid hook name before mapping
