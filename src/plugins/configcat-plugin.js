cf015ba3627b4db87188879e492fb32fe1c59d20
Chavez Harris <codedbychavez@gmail.com>
2023-07-14 17:00:51 -0300

    hook beta

--- src/plugins/configcat-plugin.js
+++ src/plugins/configcat-plugin.js
@@ -4,7 +4,7 @@ export default {
   install: (app, options) => {
     let configCatClient = configcat.createClient(options.SDKKey, options.clientOptions);
     // Augment the hooks to the client
-    if (options.hooks.length > 0) {
+    if (options.hooks) {
       for (hook of options.hook) {
         console.log('The hook: ', hook)
       }
